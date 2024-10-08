165f174ee2d119f3a059ce361b5df2edde589ebd
Bruno Luís de Oliveira <brunoliveira.ca@gmail.com>
2024-10-08 20:23:37 -0300

    refactor: Implementing the viewBinding

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,38 +1,33 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var cityNameTextView: TextView
-    private lateinit var temperatureTextView: TextView
-    private lateinit var weatherDescriptionTextView: TextView
-    private lateinit var refreshButton: Button
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        // Setting the view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Setting the listener of the system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Starting the views
-        cityNameTextView = findViewById(R.id.cityNameTextView)
-        temperatureTextView = findViewById(R.id.temperatureTextView)
-        weatherDescriptionTextView = findViewById(R.id.weatherDescriptionTextView)
-        refreshButton = findViewById(R.id.refreshButton)
-
         // Setting the listener of the button
-        refreshButton.setOnClickListener {
+        binding.refreshButton.setOnClickListener {
             // Updating the data
             updateWeatherData()
         }
@@ -44,8 +39,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateWeatherData() {
         // Updating the data
-        cityNameTextView.text = "Balneário Gaivota"
-        temperatureTextView.text = "15°C"
-        weatherDescriptionTextView.text = "Nublado"
+        binding.cityNameTextView.text = "Balneário Gaivota"
+        binding.temperatureTextView.text = "15°C"
+        binding.weatherDescriptionTextView.text = "Nublado"
     }
 }
